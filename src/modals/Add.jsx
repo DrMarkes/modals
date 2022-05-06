8586a27fb7b76a384350b71240561845eb60e2ba
DrMarkes <markovandrew@yandex.ru>
2022-05-06 23:24:54 +0700

    first commit

---
+++ src/modals/Add.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useRef } from 'react';
+import _ from 'lodash';
+import { useFormik } from 'formik';
+import { Modal, FormGroup, FormControl } from 'react-bootstrap';
+
+const generateOnSubmit = ({ setItems, onHide }) => (values) => {
+  const item = { id: _.uniqueId(), body: values.body };
+  setItems((items) => {
+    items.push(item);
+  });
+  onHide();
+};
+
+const Add = (props) => {
+  const { onHide } = props;
+  const f = useFormik({
+    onSubmit: generateOnSubmit(props),
+    initialValues: { body: '' }
+  });
+
+  const inputRef = useRef();
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+  return (
+    <Modal show>
+      <Modal.Header closeButton onHide={onHide}>
+        <Modal.Title>Add</Modal.Title>
+      </Modal.Header>
+
+      <Modal.Body>
+        <form onSubmit={f.handleSubmit}>
+          <FormGroup>
+            <FormControl
+              required
+              ref={inputRef}
+              onChange={f.handleChange}
+              onBlur={f.handleBlur}
+              value={f.values.body}
+              data-testid="input-body"
+              name="body"
+            />
+          </FormGroup>
+          <input type="submit" className="btn btn-primary mt-2" value="submit" />
+        </form>
+      </Modal.Body>
+    </Modal>
+  );
+};
+
+export default Add;
